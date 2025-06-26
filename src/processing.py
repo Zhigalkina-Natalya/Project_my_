a06342f00c54f0d0071aa14d3546cdd4b990f1c3
Zhigalkina Natalya <story.hv@gmail.com>
2025-06-26 10:49:22 +0300

    add func sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(list_of_dict: list[dict], state: str="EXECUTED") -> list[dict]:
+def filter_by_state(list_of_dict: list[dict], state: str = "EXECUTED") -> list[dict]:
     """
     Функция возвращает новый список словарей, содержащий словари, у которых ключ state
     соответствует указанному значению
@@ -10,3 +10,11 @@ def filter_by_state(list_of_dict: list[dict], state: str="EXECUTED") -> list[dic
         else:
             continue
     return new_list
+
+
+def sort_by_date(list_of_dict: list[dict], sorting: bool = True) -> list[dict]:
+    """
+    Функция возвращает новый список словарей, отсортированный по дате
+    """
+    sorted_list = sorted(list_of_dict, key=lambda x: x["date"], reverse=sorting)
+    return sorted_list
